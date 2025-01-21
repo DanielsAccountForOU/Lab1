3f57c8d9ffd480aa2deb0d994ce7c521bb53e6bc
DanielsAccountForOU <dc323824@ohio.edu>
2025-01-21 10:42:10 -0500

    Update MyTime.h

--- MyTime.h
+++ MyTime.h
@@ -61,45 +61,22 @@ class MyTime
 		int minutes;      // 0 <= minutes <= 59
  };
 
-MyTime operator + (const MyTime& t1, const MyTime& t2){
-	MyTime tmp;
-    tmp.hours = t1.hours + t2.hours;
-	tmp.minutes = t1.minutes + t2.minutes;
-	tmp.simplify();
-	return tmp;
+void MyTime::input(istream& ins){
+	/* Move the implementation code from the >> operator show below here.
+	   Remember that since this function will be a member now, there will not be a t1.
+
+	   The >> operator should now call this function.
+	   Remove the friend keyword from the operator in the .h file and move the
+	   function prototype to after the class declaration.
+	*/
+	char junk;
+	ins >> hours;
+  	ins.get(junk);
+	ins >> minutes;
+	simplify();
 }
 
-MyTime operator - (const MyTime& t1, const MyTime& t2){
-	MyTime tmp;
-	tmp.minutes = abs((t1.hours * 60 + t1.minutes) - (t2.hours * 60 + t2.minutes));
-	tmp.simplify();
-	return tmp;
-}
-
-MyTime operator / (const MyTime& t1, int num){
-	MyTime tmp;
-	tmp.minutes = t1.hours * 60 + t1.minutes;
-	tmp.minutes /= num;
-	tmp.simplify();
-	return tmp;
-}
-
-MyTime operator * (const MyTime& t1, int num){
-	MyTime tmp;
-	tmp.minutes = t1.hours * 60 + t1.minutes;
-	tmp.minutes *= num;
-	tmp.simplify();
-	return tmp;
-}
-
-bool operator == (const MyTime& t1, const MyTime& t2){
-	return t1.hours == t2.hours && t1.minutes == t2.minutes;
-}
-
-bool operator < (const MyTime& t1, const MyTime& t2){
-	return (t1.hours * 60 + t1.minutes) < (t2.hours * 60 + t2.minutes);
-}
-
-bool operator <= (const MyTime& t1, const MyTime& t2){
-	return (t1.hours * 60 + t1.minutes) <= (t2.hours * 60 + t2.minutes);
+void MyTime::output(ostream& outs){
+	/* Repeat what you did for input except using the code for the << operator */
+	outs << setfill('0') << setw(2) << hours << ':' << setfill('0') << setw(2) << minutes;
 }
